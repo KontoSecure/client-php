5062042ac0dbcdf4556e9064300b2f573d76eb98
Tschela Baumann <baumann@itopia.de>
2017-07-18 10:13:14 +0200

    Fix foreach bug in getting API key

--- KontoSecure/Response/CreateMerchant.php
+++ KontoSecure/Response/CreateMerchant.php
@@ -16,6 +16,11 @@ class CreateMerchant extends BaseResponse
     public function getApiKey()
     {
         $keys = $this->getVal('api_keys');
+        
+        if(empty($keys)) {
+            return null;
+        }
+        
         foreach($keys as $key) {
             if($key['state'] === 'active') {
                 return $key['hash'];
